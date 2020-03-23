676dde2ef02d55981e09cf003a161977c8aea63e
Jesús Mejía <jesusmejded@gmail.com>
2020-03-23 14:59:11 -0600

    improve performance

--- main.jl
+++ main.jl
@@ -5,6 +5,10 @@ using Images: channelview
 using Statistics: mean
 using Base.Iterators: partition
 import Random: randperm, seed!
+using Plots
+using BSON: @save, @load
+
+pyplot(legend=false)
 
 include("architecture.jl")
 
@@ -18,6 +22,11 @@ accuracy(m, x, y) = mean(onecold(cpu(m(x)), 1:10) .== onecold(cpu(y), 1:10))
 function mytrainer(train_set, X_test, Y_test; parm = 0.9)
     max_epochs = 30
     m = testCNN()
+
+    if isfile("mymodel.bson")
+        @load "mymodel.bson" weights
+        Flux.loadparams!(m, weights)
+    end
 
     loss(x, y) = crossentropy(m(x), y)
 
@@ -33,10 +42,16 @@ function mytrainer(train_set, X_test, Y_test; parm = 0.9)
 
     accuracies = Float64[]
     for t = 1:max_epochs
-        Flux.train!(loss, params(m), train_set, opt, cb = evalcb)
-        push!(accuracies, accuracy(m, X_test, Y_test))
+        Flux.train!(loss, params(m), train_set, opt)
+        a = accuracy(m, X_test, Y_test)
+        println("Epoch ", t, " accuracy: ", a)
+
+        push!(accuracies, a)
     end
-    m
+
+    weights = params(cpu(m))
+    # @save "mymodel.bson" weights
+    m, accuracies
 end
 
 function main()
@@ -44,12 +59,13 @@ function main()
     seed!(1)
 
     # Fetching the train and validation data and getting them into proper shape
-    N_train = 49000
-    N_test = 10000
-    I = 1:50000 #randperm(50000)
+    N_train = 5000
+    N_test = 1000
+    I = randperm(50000)
     train_set_idx = I[1:N_train]
     test_set_idx = reverse(I)[1:N_test]
 
+    println("Loading images")
     X = trainimgs(CIFAR10)
     
     imgs = [getarray(x.img) for x in X]
@@ -65,7 +81,7 @@ function main()
     train_set = gpu.([(cat(imgs[i]..., dims = 4), labels[:,i]) for i in partition(train_set_idx, 10)])
 
 
-
+    println("Loading validation images")
     test = valimgs(CIFAR10)
 
     testimgs = [getarray(test[i].img) for i in 1:100]
@@ -74,8 +90,12 @@ function main()
 
     # Print the final accuracy
 
-    m = mytrainer(train_set, X_test, Y_test)
+    println("Training...")
+    m, accuracies = mytrainer(train_set, X_test, Y_test)
+    
+    println("Testing...")
     @show(accuracy(m, X_eval, Y_eval))
+    plot(1:length(accuracies), accuracies, markershape = :o)
     # return imshow(imgs[2])
 end
 
